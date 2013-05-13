d4cc3478fba90f92f382740a4d86070535b3756e
Elusive138 <elusive138+git@gmail.com>
2013-05-14 03:45:48 +1000

    Added classes for Cells
    Includes MineCell and SafeCell, inheriting from Cell.
    
    Implements Marked and Exposed in Cell and CellValue in SafeCell.

---
+++ Minesweeper/Cell.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Minesweeper
+{
+    class Cell
+    {
+        internal bool marked;
+        internal bool exposed;
+
+        /// <summary>
+        /// Whether the cell has been marked as a mine by the player.
+        /// </summary>
+        /// <value>
+        /// Gets or sets the value of the bool field, marked.
+        /// </value>
+        public bool Marked
+        {
+            get
+            {
+                return marked;
+            }
+            set
+            {
+                marked = value;
+            }
+        }
+
+        /// <summary>
+        /// Whether the cell has been exposed by the player yet.
+        /// </summary>
+        /// <value>
+        /// Gets or sets the value of the bool field, exposed.
+        /// </value>
+        public bool Exposed
+        {
+            get
+            {
+                return exposed;
+            }
+            set
+            {
+                exposed = value;
+            }
+        }
+    }
+}
---
+++ Minesweeper/SafeCell.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Minesweeper
+{
+    class SafeCell
+    {
+        internal int cellValue;
+
+        /// <summary>
+        /// The value of the cell.
+        /// </summary>
+        /// <remarks>
+        /// This should be used to store the number of neighbouring mines.
+        /// The value must not be set to a negative number.
+        /// </remarks>
+        /// <value>
+        /// Gets or sets the value of the int field, cellValue.
+        /// </value>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// Thrown when a negative value is set.
+        /// </exception>
+        public int CellValue
+        {
+            get
+            {
+                return cellValue;
+            }
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentOutOfRangeException("Cell values must not be negative.");
+                cellValue = value;
+            }
+        }
+    }
+}
