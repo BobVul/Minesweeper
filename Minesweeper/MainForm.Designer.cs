f8e8605409fa3ddb2f830130595040b15b03bd29
Elusive138 <elusive138+git@gmail.com>
2013-05-14 02:57:06 +1000

    Set titlebar name, added drawing board

--- Minesweeper/MainForm.Designer.cs
+++ Minesweeper/MainForm.Designer.cs
@@ -28,20 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.drawingBoard = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.drawingBoard)).BeginInit();
             this.SuspendLayout();
+            // 
+            // drawingBoard
+            // 
+            this.drawingBoard.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.drawingBoard.Location = new System.Drawing.Point(0, 0);
+            this.drawingBoard.Name = "drawingBoard";
+            this.drawingBoard.Size = new System.Drawing.Size(284, 261);
+            this.drawingBoard.TabIndex = 0;
+            this.drawingBoard.TabStop = false;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.drawingBoard);
             this.Name = "MainForm";
-            this.Text = "MainForm";
+            this.Text = "Minesweeper";
+            ((System.ComponentModel.ISupportInitialize)(this.drawingBoard)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox drawingBoard;
     }
 }
 
